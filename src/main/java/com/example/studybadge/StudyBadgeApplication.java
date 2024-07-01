9bb79afb7075fb6ed5b07ee861e011234c162932
HanSeul <w0w1278@naver.com>
2024-07-01 10:43:01 +0900

    init setting

---
+++ src/main/java/com/example/studybadge/StudyBadgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.studybadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBadgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyBadgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studybadge/StudyBadgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studybadge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBadgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
