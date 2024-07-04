2ac716d9b573f59f8d5977308415983ef0e30336
Ahn <youn_tip@naver.com>
2024-07-04 21:24:56 +0900

    chore: STD-2 SecurityConfig 수정

--- src/main/java/com/tenten/studybadge/common/config/SecurityConfig.java
+++ src/main/java/com/tenten/studybadge/common/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
 
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( requests -> requests
-                        .requestMatchers("/api/members/sign-up", "/api/members/auth/**", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/members/login/**", "/api/members/logout/**").permitAll())
+                        .requestMatchers("/api/members/sign-up", "/api/members/auth/**", "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/members/login/**").permitAll())
 
                 .headers(headers -> headers // h2-console 페이지 접속을 위한 설정
                         .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
