efb00276d4013a8dce38b1e2497f641f9c505fc8
HanSeul <w0w1278@naver.com>
2024-07-08 23:03:37 +0900

    test: STD-16 일정 등록 service 테스트

--- src/main/java/com/tenten/studybadge/schedule/dto/RepeatScheduleCreateRequest.java
+++ src/main/java/com/tenten/studybadge/schedule/dto/RepeatScheduleCreateRequest.java
@@ -6,6 +6,7 @@ import com.tenten.studybadge.common.jsondeserializer.RepeatSituationNumberDeseri
 import com.tenten.studybadge.type.schedule.RepeatCycle;
 import com.tenten.studybadge.type.schedule.RepeatSituation;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,19 @@ public class RepeatScheduleCreateRequest extends ScheduleCreateRequest {
   @JsonDeserialize(using = RepeatSituationNumberDeserializer.class)
   private RepeatSituation repeatSituation;
   private LocalDate repeatEndDate;
+
+  public RepeatScheduleCreateRequest(String scheduleName, String scheduleContent,
+      LocalDate startDate, LocalTime startTime, LocalTime endTime,
+      RepeatCycle repeatCycle, RepeatSituation repeatSituation, LocalDate repeatEndDate,
+      Long placeId) {
+    this.scheduleName = scheduleName;
+    this.scheduleContent = scheduleContent;
+    this.scheduleDate = startDate;
+    this.scheduleStartTime = startTime;
+    this.scheduleEndTime = endTime;
+    this.placeId = placeId;
+    this.repeatCycle = repeatCycle;
+    this.repeatSituation = repeatSituation;
+    this.repeatEndDate = repeatEndDate;
+  }
 }
