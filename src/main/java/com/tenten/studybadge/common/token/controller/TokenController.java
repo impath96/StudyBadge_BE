2f50a733237d5f707d08b52de133b164dfc0f9ee
Ahn <youn_tip@naver.com>
2024-07-09 23:36:58 +0900

    style: import 삭제

--- src/main/java/com/tenten/studybadge/common/token/controller/TokenController.java
+++ src/main/java/com/tenten/studybadge/common/token/controller/TokenController.java
@@ -1,14 +1,11 @@
 package com.tenten.studybadge.common.token.controller;
 
 import com.tenten.studybadge.common.token.service.TokenService;
-import com.tenten.studybadge.common.utils.CookieUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
