068863135507f045f2acb7fdee89ba9f3984815a
HanSeul <w0w1278@naver.com>
2024-07-10 19:14:25 +0900

    refactor: json에서 origing type 소문자로도 가능 하도록 역직렬화 추가

--- src/main/java/com/tenten/studybadge/schedule/dto/ScheduleEditRequest.java
+++ src/main/java/com/tenten/studybadge/schedule/dto/ScheduleEditRequest.java
@@ -2,6 +2,9 @@ package com.tenten.studybadge.schedule.dto;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.tenten.studybadge.common.jsondeserializer.RepeatSituationNumberDeserializer;
+import com.tenten.studybadge.common.jsondeserializer.ScheduleOriginTypeDeserializer;
 import com.tenten.studybadge.type.schedule.ScheduleOriginType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -24,6 +27,7 @@ public abstract class ScheduleEditRequest {
   @NotNull(message = "수정할 일정 id는 필수입니다.")
   protected long scheduleId;
   @NotNull(message = "기존 일정 타입은 필수입니다.")
+  @JsonDeserialize(using = ScheduleOriginTypeDeserializer.class)
   protected ScheduleOriginType originType;
   @NotBlank(message = "일정 이름은 필수입니다.")
   protected String scheduleName;
