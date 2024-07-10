02f44d811d76fa6d6e65ab618ffdef2a62bf34ce
HanSeul <w0w1278@naver.com>
2024-07-10 22:13:37 +0900

    style: ERROR_CODE와 ERROR_MESSAGE 상수로 변경

--- src/main/java/com/tenten/studybadge/common/exception/schedule/IllegalArgumentForRepeatScheduleEditRequestException.java
+++ src/main/java/com/tenten/studybadge/common/exception/schedule/IllegalArgumentForRepeatScheduleEditRequestException.java
@@ -5,16 +5,19 @@ import org.springframework.http.HttpStatus;
 
 public class IllegalArgumentForRepeatScheduleEditRequestException extends AbstractException {
 
+    private static final String ERROR_CODE = "ILLEGAL_ARGUMENT_FOR_REPEAT_SCHEDULE_EDIT_REQUEST";
+    private static final String ERROR_MESSAGE = "올바르지 않은 반복 일정 수정 요청 값입니다.";
+
     @Override
     public HttpStatus getHttpStatus() {
         return HttpStatus.BAD_REQUEST;
     }
     @Override
     public String getErrorCode() {
-        return "ILLEGAL_ARGUMENT_FOR_REPEAT_SCHEDULE_EDIT_REQUEST";
+        return ERROR_CODE;
     }
     @Override
     public String getMessage() {
-        return "올바르지 않은 반복 일정 수정 요청 값입니다.";
+        return ERROR_MESSAGE ;
     }
 }
--- src/main/java/com/tenten/studybadge/common/exception/schedule/IllegalArgumentForRepeatSituationException.java
+++ src/main/java/com/tenten/studybadge/common/exception/schedule/IllegalArgumentForRepeatSituationException.java
@@ -4,6 +4,8 @@ import com.tenten.studybadge.common.exception.basic.AbstractException;
 import org.springframework.http.HttpStatus;
 
 public class IllegalArgumentForRepeatSituationException extends AbstractException {
+    private static final String ERROR_CODE = "ILLEGAL_ARGUMENT_FOR_REPEAT_SITUATION";
+    private static final String ERROR_MESSAGE = "올바르지 않은 월간 반복 요청 값입니다.";
 
     @Override
     public HttpStatus getHttpStatus() {
@@ -11,10 +13,10 @@ public class IllegalArgumentForRepeatSituationException extends AbstractExceptio
     }
     @Override
     public String getErrorCode() {
-        return "ILLEGAL_ARGUMENT_FOR_REPEAT_SITUATION";
+        return ERROR_CODE;
     }
     @Override
     public String getMessage() {
-        return "올바르지 않은 월간 반복 요청 값입니다.";
+        return ERROR_MESSAGE ;
     }
 }
