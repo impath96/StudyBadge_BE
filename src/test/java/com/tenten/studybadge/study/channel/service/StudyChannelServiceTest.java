287c6c2be9481fba512de190b39d33281ff19aa5
impath96 <impath96@gmail.com>
2024-07-13 14:21:18 +0900

    test: 스터디 채널 목록 조회 테스트 작성
    - 많은 데이터를 필요로 하는 만큼 조금 더 효율적으로 데이터를 세팅하는 방법을 알아야 할 것 같다
    - 현재는 "최신순" 하나에 대해서만 조회 테스트 작성

--- src/test/java/com/tenten/studybadge/study/channel/service/StudyChannelServiceTest.java
+++ src/test/java/com/tenten/studybadge/study/channel/service/StudyChannelServiceTest.java
@@ -4,9 +4,13 @@ import com.tenten.studybadge.common.exception.studychannel.InvalidStudyDurationE
 import com.tenten.studybadge.common.exception.studychannel.InvalidStudyStartDateException;
 import com.tenten.studybadge.member.domain.entity.Member;
 import com.tenten.studybadge.member.domain.repository.MemberRepository;
+import com.tenten.studybadge.study.channel.domain.entity.Recruitment;
 import com.tenten.studybadge.study.channel.domain.entity.StudyChannel;
+import com.tenten.studybadge.study.channel.domain.entity.StudyDuration;
 import com.tenten.studybadge.study.channel.domain.repository.StudyChannelRepository;
+import com.tenten.studybadge.study.channel.dto.SearchCondition;
 import com.tenten.studybadge.study.channel.dto.StudyChannelCreateRequest;
+import com.tenten.studybadge.study.channel.dto.StudyChannelListResponse;
 import com.tenten.studybadge.study.member.domain.entity.StudyMember;
 import com.tenten.studybadge.study.member.domain.repository.StudyMemberRepository;
 import com.tenten.studybadge.type.study.channel.Category;
@@ -21,8 +25,11 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -139,6 +146,110 @@ class StudyChannelServiceTest {
                     .isExactlyInstanceOf(InvalidStudyStartDateException.class)
                     .hasMessage("스터디 시작 날짜는 오늘 날짜 이후로 설정해주세요.");
         }
+    }
+
+    // [ Query ]
+    @DisplayName("[스터디 채널 목록 조회 테스트]")
+    @Nested
+    class getStudyChannelsTest {
+
+        @DisplayName("스터디 채널 목록 조회 - 최신순")
+        @Test
+        void getStudyChannels() {
+
+            Member member1 = Member.builder().id(1L).name("회원 1").build();
+            Member member2 = Member.builder().id(2L).name("회원 2").build();
+            Member member3 = Member.builder().id(3L).name("회원 3").build();
+            LocalDate now = LocalDate.now();
+            StudyChannel studyChannel1 = StudyChannel.builder()
+                    .id(1L)
+                    .name("스터디명")
+                    .description("스터디 설명")
+                    .studyDuration(StudyDuration.builder()
+                            .studyStartDate(now.plusDays(2))
+                            .studyEndDate(now.plusMonths(4))
+                            .build())
+                    .recruitment(Recruitment.builder()
+                            .recruitmentNumber(6)
+                            .recruitmentStatus(RecruitmentStatus.RECRUITING)
+                            .build())
+                    .category(Category.IT)
+                    .region("")
+                    .meetingType(MeetingType.ONLINE)
+                    .chattingUrl("오픈채팅방 URL")
+                    .deposit(10_000)
+                    .viewCnt(4)
+                    .build();
+            StudyChannel studyChannel2 = StudyChannel.builder()
+                    .id(2L)
+                    .name("스터디명2")
+                    .description("스터디 설명2")
+                    .studyDuration(StudyDuration.builder()
+                            .studyStartDate(now.plusDays(2))
+                            .studyEndDate(now.plusMonths(4))
+                            .build())
+                    .recruitment(Recruitment.builder()
+                            .recruitmentNumber(5)
+                            .recruitmentStatus(RecruitmentStatus.RECRUITING)
+                            .build())
+                    .category(Category.IT)
+                    .region("")
+                    .meetingType(MeetingType.ONLINE)
+                    .chattingUrl("오픈채팅방 URL")
+                    .deposit(10_000)
+                    .viewCnt(1)
+                    .build();
+            StudyChannel studyChannel3 = StudyChannel.builder()
+                    .id(3L)
+                    .name("스터디명3")
+                    .description("스터디 설명3")
+                    .studyDuration(StudyDuration.builder()
+                            .studyStartDate(now.plusDays(2))
+                            .studyEndDate(now.plusMonths(4))
+                            .build())
+                    .recruitment(Recruitment.builder()
+                            .recruitmentNumber(6)
+                            .recruitmentStatus(RecruitmentStatus.RECRUITING)
+                            .build())
+                    .category(Category.EMPLOYMENT)
+                    .region("")
+                    .meetingType(MeetingType.ONLINE)
+                    .chattingUrl("오픈채팅방 URL")
+                    .deposit(10_000)
+                    .viewCnt(2)
+                    .build();
+
+            StudyMember leader1 = StudyMember.leader(member1, studyChannel1);
+            StudyMember leader2 = StudyMember.leader(member2, studyChannel2);
+            StudyMember leader3 = StudyMember.leader(member3, studyChannel3);
+            studyChannel1.getStudyMembers().add(leader1);
+            studyChannel2.getStudyMembers().add(leader2);
+            studyChannel3.getStudyMembers().add(leader3);
+
+            Pageable pageable = PageRequest.of(0, 6, Sort.by(Sort.Direction.DESC, "createdAt"));
+            SearchCondition searchCondition = new SearchCondition(MeetingType.ONLINE, RecruitmentStatus.RECRUITING, Category.IT);
+            PageImpl<StudyChannel> page = new PageImpl<>(List.of(studyChannel1, studyChannel2, studyChannel3), pageable, 3);
+            given(studyChannelRepository.findAll(any(Specification.class), any(Pageable.class))).willReturn(page);
+
+            List<Long> ids = page.getContent().stream()
+                    .map(StudyChannel::getId)
+                    .toList();
+
+            given(studyMemberRepository.findAllWithLeader(ids, StudyMemberRole.LEADER))
+                    .willReturn(List.of(leader1, leader2, leader3));
+
+            StudyChannelListResponse response = studyChannelService.getStudyChannels(pageable, searchCondition);
+
+            assertThat(response.getTotalPage()).isEqualTo(1);
+            assertThat(response.getPageSize()).isEqualTo(6);
+            assertThat(response.getPageNumber()).isEqualTo(1);
+            assertThat(response.getTotalCount()).isEqualTo(3);
+            assertThat(response.getStudyChannels().size()).isEqualTo(3);
+            assertThat(response.getStudyChannels().get(0).getStudyChannelId()).isEqualTo(studyChannel1.getId());
+            assertThat(response.getStudyChannels().get(0).getMemberId()).isEqualTo(leader1.getMember().getId());
+            assertThat(response.getStudyChannels().get(0).getMemberName()).isEqualTo(leader1.getMember().getName());
+        }
+
     }
 
 }
