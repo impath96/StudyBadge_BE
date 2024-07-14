fa66c50bcba10342f5dc19e2a33878528c786098
HanSeul <w0w1278@naver.com>
2024-07-15 00:10:24 +0900

    style: test 코드시 작성하기 편하기 위해 builder 어노테이션을 request에 추가

--- src/main/java/com/tenten/studybadge/schedule/dto/RepeatScheduleCreateRequest.java
+++ src/main/java/com/tenten/studybadge/schedule/dto/RepeatScheduleCreateRequest.java
@@ -10,11 +10,13 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RepeatScheduleCreateRequest {
