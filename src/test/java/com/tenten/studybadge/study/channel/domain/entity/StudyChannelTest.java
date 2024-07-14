90fa37cc4db2a41e363537a0d48284bb27a7bb7f
impath96 <impath96@gmail.com>
2024-07-14 17:56:40 +0900

    test: 리더가 아닌 경우 테스트 추가 및 공통 데이터 생성
    - 리더가 아닌 경우에 대한 테스트 코드 추가
    - 회원 및 스터디 채널 생성 로직을 @BeforeEach로 초기화하도록 변경

--- src/test/java/com/tenten/studybadge/study/channel/domain/entity/StudyChannelTest.java
+++ src/test/java/com/tenten/studybadge/study/channel/domain/entity/StudyChannelTest.java
@@ -5,23 +5,27 @@ import com.tenten.studybadge.study.member.domain.entity.StudyMember;
 import com.tenten.studybadge.type.study.channel.Category;
 import com.tenten.studybadge.type.study.channel.MeetingType;
 import com.tenten.studybadge.type.study.channel.RecruitmentStatus;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class StudyChannelTest {
 
-    @DisplayName("특정 회원이 해당 스터디 채널의 리더일 경우 True를 반환한다.")
-    @Test
-    void isLeader_returnTrue() {
+    Member member1;
+    Member member2;
+    StudyChannel studyChannel;
 
-        Member member1 = Member.builder().id(1L).name("회원 1").build();
-        Member member2 = Member.builder().id(2L).name("회원 2").build();
+    @BeforeEach
+    void setUp() {
+        member1 = Member.builder().id(1L).name("회원 1").build();
+        member2 = Member.builder().id(2L).name("회원 2").build();
 
         LocalDate now = LocalDate.now();
-        StudyChannel studyChannel = StudyChannel.builder()
+        studyChannel = StudyChannel.builder()
                 .id(1L)
                 .name("스터디명")
                 .description("스터디 설명")
@@ -41,14 +45,33 @@ class StudyChannelTest {
                 .viewCnt(4)
                 .build();
 
+    }
+
+    @DisplayName("특정 회원이 해당 스터디 채널의 리더일 경우 True를 반환한다.")
+    @Test
+    void isLeader_returnTrue() {
+
+        StudyMember leader = StudyMember.leader(member1, studyChannel);
+        studyChannel.getStudyMembers().add(leader);
+
+        boolean actual = studyChannel.isLeader(member1);
+
+        assertThat(actual).isTrue();
+
+    }
+
+    @DisplayName("해당 스터디 채널의 리더가 아닐 경우 경우 False를 반환한다.")
+    @Test
+    void isLeader_returnFalse() {
+
         StudyMember leader = StudyMember.leader(member1, studyChannel);
         StudyMember member = StudyMember.member(member2, studyChannel);
         studyChannel.getStudyMembers().add(leader);
         studyChannel.getStudyMembers().add(member);
 
-        boolean actual = studyChannel.isLeader(member1);
+        boolean actual = studyChannel.isLeader(member2);
 
-        Assertions.assertThat(actual).isTrue();
+        assertThat(actual).isFalse();
 
     }
 
