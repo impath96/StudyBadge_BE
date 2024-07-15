35e11a38e806e6c0b782a3f39c6ec6c327708c15
impath96 <impath96@gmail.com>
2024-07-15 10:40:05 +0900

    style: SecurityConfig 경로 구체적인 path로 변경

--- src/main/java/com/tenten/studybadge/common/config/SecurityConfig.java
+++ src/main/java/com/tenten/studybadge/common/config/SecurityConfig.java
@@ -35,8 +35,8 @@ public class SecurityConfig {
 
         return http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests( requests -> requests
-                        .requestMatchers("/api/members/sign-up", "/api/members/auth/", "/h2-console/", "/swagger-ui/", "/v3/api-docs/", "/api/members/login/", "/error", "/health-check").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/study-channels/**").permitAll()
+                        .requestMatchers("/api/members/sign-up", "/api/members/auth/**", "/h2-console/**", "/swagger-ui/", "/v3/api-docs/**", "/api/members/login/**", "/error", "/health-check").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/study-channels", "/api/study-channels/{studyChannelId:\\d+}").permitAll()
                         .requestMatchers("/api/members/logout", "api/study-channels/*/places", "/api/study-channels/**", "/api/token/re-issue").hasRole("USER"))
 
                 .cors(cors -> new CorsConfig())
