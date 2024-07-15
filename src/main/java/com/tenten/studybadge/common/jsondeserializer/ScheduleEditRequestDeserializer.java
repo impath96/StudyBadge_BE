c977a9e15460c3fe6307d28a82e3cdd56c6b99b1
HanSeul <w0w1278@naver.com>
2024-07-16 00:16:02 +0900

    refactor: STD-18 일정 수정 로직 변경 - Controller, reqeust 값 - scheduleEditReqeust를 추상 클래스로 둔 것 삭제 - scheduleeditReqeust를 인터페이스로 만들고 editType을 역직렬화로 구분하여 단일 일정/ 반복 일정으로 변경 요청 - 기존 ScheduleOriginType -> ScheduleType으로 변경해 재사용성을 높임 - 반복 일정 -> 단일 일정으로 수정할 때는 request 값을 SingleScheduleEditRequest로 받아 service로직에서 구분 없앰

---
+++ src/main/java/com/tenten/studybadge/common/jsondeserializer/ScheduleEditRequestDeserializer.java
@@ -0,0 +1,78 @@
+package com.tenten.studybadge.common.jsondeserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.tenten.studybadge.common.exception.schedule.IllegalArgumentForScheduleEditRequestException;
+import com.tenten.studybadge.schedule.dto.RepeatScheduleEditRequest;
+import com.tenten.studybadge.schedule.dto.ScheduleEditRequest;
+import com.tenten.studybadge.schedule.dto.SingleScheduleEditRequest;
+import com.tenten.studybadge.type.schedule.RepeatCycle;
+import com.tenten.studybadge.type.schedule.RepeatSituation;
+import com.tenten.studybadge.type.schedule.ScheduleType;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class ScheduleEditRequestDeserializer extends JsonDeserializer<ScheduleEditRequest> {
+
+  @Override
+  public ScheduleEditRequest deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    ObjectMapper mapper = (ObjectMapper) p.getCodec();
+    JsonNode node = mapper.readTree(p);
+    ScheduleType editType = ScheduleType.valueOf(node.get("editType").asText().toUpperCase());
+
+    if (ScheduleType.SINGLE == editType) {
+      return createSingleScheduleEditRequest(node);
+    } else if (ScheduleType.REPEAT == editType) {
+      return createRepeatScheduleEditRequest(node);
+    }
+
+    throw new IllegalArgumentForScheduleEditRequestException();
+  }
+
+  private SingleScheduleEditRequest createSingleScheduleEditRequest(JsonNode node) {
+    return SingleScheduleEditRequest.builder()
+        .scheduleId(node.get("scheduleId").asLong())
+        .originType(ScheduleType.valueOf(node.get("originType").asText().toUpperCase()))
+        .editType(ScheduleType.valueOf(node.get("editType").asText().toUpperCase()))
+        .scheduleName(node.get("scheduleName").asText())
+        .scheduleContent(node.get("scheduleContent").asText())
+        .selectedDate(LocalDate.parse(node.get("selectedDate").asText()))
+        .scheduleStartTime(LocalTime.parse(node.get("scheduleStartTime").asText()))
+        .scheduleEndTime(LocalTime.parse(node.get("scheduleEndTime").asText()))
+        .placeId(node.has("placeId") ? node.get("placeId").asLong() : null)
+        .build();
+  }
+
+  private RepeatScheduleEditRequest createRepeatScheduleEditRequest(JsonNode node) {
+    return RepeatScheduleEditRequest.builder()
+        .scheduleId(node.get("scheduleId").asLong())
+        .originType(ScheduleType.valueOf(node.get("originType").asText().toUpperCase()))
+        .editType(ScheduleType.valueOf(node.get("editType").asText().toUpperCase()))
+        .scheduleName(node.get("scheduleName").asText())
+        .scheduleContent(node.get("scheduleContent").asText())
+        .selectedDate(LocalDate.parse(node.get("selectedDate").asText()))
+        .scheduleStartTime(LocalTime.parse(node.get("scheduleStartTime").asText()))
+        .scheduleEndTime(LocalTime.parse(node.get("scheduleEndTime").asText()))
+        .placeId(node.has("placeId") ? node.get("placeId").asLong() : null)
+        .repeatCycle(RepeatCycle.valueOf(node.get("repeatCycle").asText().toUpperCase()))
+        .repeatSituation(deserializeRepeatSituation(node.get("repeatSituation")))
+        .repeatEndDate(LocalDate.parse(node.get("repeatEndDate").asText()))
+        .build();
+  }
+
+  private RepeatSituation deserializeRepeatSituation(JsonNode node) {
+    if (node.isInt()) {
+      return RepeatSituation.fromInt(node.asInt());
+    } else if (node.isTextual()) {
+      return RepeatSituation.fromString(node.asText());
+    }
+    throw new IllegalArgumentException("Invalid RepeatSituation value");
+  }
+}
+
