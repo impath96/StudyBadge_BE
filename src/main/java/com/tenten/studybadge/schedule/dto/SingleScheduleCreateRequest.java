6936225cbc1d375b87fa8f8691f746d8ae2d9c3e
HanSeul <w0w1278@naver.com>
2024-07-16 15:39:51 +0900

    style: 테스트 코드의 가독성을 위해 단일 일정 생성 요청 builder 패턴 추가

--- src/main/java/com/tenten/studybadge/schedule/dto/SingleScheduleCreateRequest.java
+++ src/main/java/com/tenten/studybadge/schedule/dto/SingleScheduleCreateRequest.java
@@ -5,10 +5,12 @@ import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
+@Builder
 @AllArgsConstructor
 public class SingleScheduleCreateRequest {
     @NotNull(message = "회원 id는 필수입니다.")
