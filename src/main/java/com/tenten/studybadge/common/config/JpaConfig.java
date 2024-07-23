c0692085707bd4d2144aa9493815b52976e246de
HanSeul <w0w1278@naver.com>
2024-07-23 16:46:54 +0900

    chore: EnableJpaAuditing JpaConfig에 따로 설정함
    - main SpringbootApplication에서 지움
    - controller 테스트(WebMvcTest)을 할때는 Service, Repository, Component 등은 빈으로 등록하지 않는다, 그러나 main에있는 EnableJpaAuditing어노테이션으로 인해 JPA 관련 빈을 찾으려 하기 때문에 Jpa관련 설정 클래스를 분리해야함

---
+++ src/main/java/com/tenten/studybadge/common/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.tenten.studybadge.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
