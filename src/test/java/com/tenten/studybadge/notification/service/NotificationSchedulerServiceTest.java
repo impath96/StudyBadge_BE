f7518ee31ae92f0b5dc7694ade81c96b0cf026d5
HanSeul <w0w1278@naver.com>
2024-07-26 14:16:08 +0900

    test: STD-20 빌드시 fail되는 test 주석처리 설정

--- src/test/java/com/tenten/studybadge/notification/service/NotificationSchedulerServiceTest.java
+++ src/test/java/com/tenten/studybadge/notification/service/NotificationSchedulerServiceTest.java
@@ -70,40 +70,40 @@ public class NotificationSchedulerServiceTest {
         assertNull(nextFireTime, "Trigger는 종료 날짜 이후에 실행되지 않아야 합니다");
     }
 
-    @Test
-    @DisplayName("그제 - 어제 일간 반복하는 일정이 오늘에 일정이 안울리는지 테스트 : if문 생략했을때")
-    public void testSchedulingRepeatScheduleNotification_deleteIf() throws SchedulerException {
-        // 본 테스트는 NotificaitionSchedulerService.java
-        // schedulingRepeatScheduleNotification메서드에 있는 if문을 생략해야 통과됩니다. > 현재 Line 81: 줄은 바뀔 수도 있음
-        // given
-        RepeatSchedule repeatSchedule = mock(RepeatSchedule.class);
-        StudyChannel studyChannel = mock(StudyChannel.class);
-
-        when(studyChannel.getId()).thenReturn(1L);
-        when(repeatSchedule.getStudyChannel()).thenReturn(studyChannel);
-        when(repeatSchedule.getScheduleDate()).thenReturn(LocalDate.now().minusDays(2));
-        when(repeatSchedule.getScheduleStartTime()).thenReturn(LocalTime.of(12, 0));
-        when(repeatSchedule.getRepeatEndDate()).thenReturn(LocalDate.now().minusDays(1)); // 어제
-        when(repeatSchedule.getRepeatCycle()).thenReturn(RepeatCycle.DAILY);
-
-        // Trigger 캡처
-        ArgumentCaptor<Trigger> triggerCaptor = ArgumentCaptor.forClass(Trigger.class);
-
-        // when
-        notificationSchedulerService.schedulingRepeatScheduleNotification(repeatSchedule);
-
-        // then
-        // 스케줄러가 올바른 트리거로 호출되었는지 검증
-        verify(scheduler).scheduleJob(any(JobDetail.class), triggerCaptor.capture());
-        Trigger trigger = triggerCaptor.getValue();
-
-        // 종료 날짜가 올바른지 검증
-        Date expectedEndDate = Date.from(LocalDate.now().minusDays(1).atTime(23, 59, 59).atZone(ZoneId.systemDefault()).toInstant());
-        assertEquals(expectedEndDate, trigger.getEndTime());
-
-        // 종료 날짜 이후 트리거가 실행되지 않는지 검증
-        Date afterEndDate = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date nextFireTime = trigger.getFireTimeAfter(afterEndDate);
-        assertNull(nextFireTime, "Trigger는 종료 날짜 이후에 실행되지 않아야 합니다");
-    }
+//    @Test
+//    @DisplayName("그제 - 어제 일간 반복하는 일정이 오늘에 일정이 안울리는지 테스트 : if문 생략했을때")
+//    public void testSchedulingRepeatScheduleNotification_deleteIf() throws SchedulerException {
+//        // 본 테스트는 NotificaitionSchedulerService.java
+//        // schedulingRepeatScheduleNotification메서드에 있는 if문을 생략해야 통과됩니다. > 현재 Line 81: 줄은 바뀔 수도 있음
+//        // given
+//        RepeatSchedule repeatSchedule = mock(RepeatSchedule.class);
+//        StudyChannel studyChannel = mock(StudyChannel.class);
+//
+//        when(studyChannel.getId()).thenReturn(1L);
+//        when(repeatSchedule.getStudyChannel()).thenReturn(studyChannel);
+//        when(repeatSchedule.getScheduleDate()).thenReturn(LocalDate.now().minusDays(2));
+//        when(repeatSchedule.getScheduleStartTime()).thenReturn(LocalTime.of(12, 0));
+//        when(repeatSchedule.getRepeatEndDate()).thenReturn(LocalDate.now().minusDays(1)); // 어제
+//        when(repeatSchedule.getRepeatCycle()).thenReturn(RepeatCycle.DAILY);
+//
+//        // Trigger 캡처
+//        ArgumentCaptor<Trigger> triggerCaptor = ArgumentCaptor.forClass(Trigger.class);
+//
+//        // when
+//        notificationSchedulerService.schedulingRepeatScheduleNotification(repeatSchedule);
+//
+//        // then
+//        // 스케줄러가 올바른 트리거로 호출되었는지 검증
+//        verify(scheduler).scheduleJob(any(JobDetail.class), triggerCaptor.capture());
+//        Trigger trigger = triggerCaptor.getValue();
+//
+//        // 종료 날짜가 올바른지 검증
+//        Date expectedEndDate = Date.from(LocalDate.now().minusDays(1).atTime(23, 59, 59).atZone(ZoneId.systemDefault()).toInstant());
+//        assertEquals(expectedEndDate, trigger.getEndTime());
+//
+//        // 종료 날짜 이후 트리거가 실행되지 않는지 검증
+//        Date afterEndDate = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+//        Date nextFireTime = trigger.getFireTimeAfter(afterEndDate);
+//        assertNull(nextFireTime, "Trigger는 종료 날짜 이후에 실행되지 않아야 합니다");
+//    }
 }
