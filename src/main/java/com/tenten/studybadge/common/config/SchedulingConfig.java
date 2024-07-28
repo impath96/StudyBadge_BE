3420ecaa68b329d5d1d9e6978027032841e0751b
HanSeul <w0w1278@naver.com>
2024-07-28 10:56:25 +0900

    feat: 스케줄링 가능하도록 config 설정

---
+++ src/main/java/com/tenten/studybadge/common/config/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.tenten.studybadge.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+  // 필요한 설정이 있으면 추가
+}
