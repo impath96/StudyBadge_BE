a84f5defcaa88c3d9386c1f707fa8fc43a464201
Ahn <youn_tip@naver.com>
2024-07-29 00:41:04 +0900

    chore: STD-4 링크 재수정
    - 프론트 링크 수정

--- src/main/java/com/tenten/studybadge/common/constant/MailConstant.java
+++ src/main/java/com/tenten/studybadge/common/constant/MailConstant.java
@@ -8,11 +8,11 @@ public class MailConstant {
 
     public static final String UNICODE = "UTF-8";
 
-    public static final String BASE_URL = "http://localhost:5173";
+    public static final String BASE_URL = "http://localhost:5173/SignUp/mailAuth";
     public static final String SIGNUP_BODY =  """
                                                 <h3> 안녕하세요, Study Badge 가입을 환영합니다.
                                                 아래 링크를 클릭해, 인증을 완료해주세요.</h3>
-                                                <div><a href="%s/api/members/auth?email=%s&code=%s">
+                                                <div><a href="%s?email=%s&code=%s">
                                                 인증하기 </a></div>
                                               """;
 
