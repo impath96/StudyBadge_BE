37681a8ddc21e8a10a1bfa71087ee92ecbc24509
Ahn <youn_tip@naver.com>
2024-07-30 17:00:18 +0900

    style: STD-4 링크 주소 변경

--- src/main/java/com/tenten/studybadge/common/constant/MailConstant.java
+++ src/main/java/com/tenten/studybadge/common/constant/MailConstant.java
@@ -8,7 +8,7 @@ public class MailConstant {
 
     public static final String UNICODE = "UTF-8";
 
-    public static final String BASE_URL = "http://localhost:5173/SignUp/mailAuth";
+    public static final String BASE_URL = "https://study-badge.vercel.app/SignUp/mailAuth";
     public static final String SIGNUP_BODY =  """
                                                 <h3> 안녕하세요, Study Badge 가입을 환영합니다.
                                                 아래 링크를 클릭해, 인증을 완료해주세요.</h3>
@@ -19,7 +19,7 @@ public class MailConstant {
     public static final String RESET_PASSWORD_BODY =  """
                                                 <h3> 안녕하세요, Study Badge 비밀번호 변경 메일입니다..
                                                 아래 링크를 클릭해, 인증코드를 입력하여 인증을 완료해주시고, 비밀번호를 변경해주세요..</h3>
-                                                <div><a href="http://localhost:5173/resetPassword?email=%s">
+                                                <div><a href="https://study-badge.vercel.app/resetPassword?email=%s">
                                                 변경 링크 </a></div> 
                                               """;
 }
